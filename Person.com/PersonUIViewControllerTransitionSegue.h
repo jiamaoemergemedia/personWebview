2872f0302f94a1ab25844168a78e60c6b8d3e837
Jiamao Zheng <jiamao@emergemedia.com>
2015-10-23 16:23:42 -0500

    ADD TR

---
+++ Person.com/PersonUIViewControllerTransitionSegue.h
@@ -0,0 +1,13 @@
+//
+//  PersonUIViewControllerTransitionSegue.h
+//  Person
+//
+//  Created by Jiamao Zheng on 10/9/15.
+//  Copyright (c) 2015 Emerge Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonUIViewControllerTransitionSegue : UIStoryboardSegue
+
+@end
---
+++ Person.com/PersonUIViewControllerTransitionSegue.m
@@ -0,0 +1,27 @@
+//
+//  PersonUIViewControllerTransitionSegue.m
+//  Person
+//
+//  Created by Jiamao Zheng on 10/9/15.
+//  Copyright (c) 2015 Emerge Media. All rights reserved.
+//
+
+#import "PersonUIViewControllerTransitionSegue.h"
+
+@implementation PersonUIViewControllerTransitionSegue
+
+-(void)perform
+{
+    UIViewController *sourceViewController = [self sourceViewController];
+    UIViewController *destinationViewController = [self destinationViewController];
+    
+    CATransition *transition = [CATransition animation];
+    transition.duration = 0.5;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionDefault];
+    transition.type = kCATransitionPush;
+    transition.subtype = kCATransitionFromRight;
+//    [sourceViewController.view.layer addAnimation:transition forKey:kCATransition];
+    [sourceViewController presentViewController:destinationViewController animated:NO completion:nil];
+}
+
+@end
